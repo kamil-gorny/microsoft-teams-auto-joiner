aacf1f1a7ef89416e143a686ac7f4ccca542b0ed
Kamil <kamil.gorny99@gmail.com>
2020-11-03 16:10:18 +0100

    chore(*): Add .gitignore

--- autojoiner.py
+++ autojoiner.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-def main():
-   driver = webdriver.Chrome(PATH)
-  
-main()
+
+driver = webdriver.Chrome(PATH)
+driver.get("https://teams.microsoft.com")
+
+
+
