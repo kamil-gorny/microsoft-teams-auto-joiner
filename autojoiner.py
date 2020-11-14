d235ec2ffe6a1b7d0e39137f35726ea920bbb6b7
Kamil <kamil.gorny99@gmail.com>
2020-11-14 21:35:54 +0100

    fix(): Correctly wait for elements to bo clickable

--- autojoiner.py
+++ autojoiner.py
@@ -9,29 +9,36 @@ import time
 
 script, email, password = argv
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-
-driver = webdriver.Chrome(PATH)
+options = webdriver.ChromeOptions() 
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(options=options, executable_path=PATH)
 
 driver.get("https://teams.microsoft.com")
 
 def try_locating_element(id):
     try:
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, id))
+            EC.element_to_be_clickable((By.XPATH, id))
         )
     except:
         print("Nie zlokalizowano")
         driver.quit()
 
-try_locating_element('i0116')
-
+try_locating_element("//input[@id='i0116']")
 login_form = driver.find_element_by_xpath("//input[@id='i0116']")
-login_form.send_keys(email)
-login_form.send_keys(Keys.RETURN)
+login_form.send_keys(email,Keys.RETURN)
 
-# try_locating_element('i0118')
 
+try_locating_element("//input[@id='i0118']")
 password_form = driver.find_element_by_xpath("//input[@id='i0118']")
-password_form.send_keys(password)
+password_form.send_keys(password, Keys.RETURN)
+# password_form.send_keys(Keys.RETURN)
+
+try_locating_element("//a[@class='use-app-lnk']")
+use_web_app = driver.find_element_by_xpath("//a[@class='use-app-lnk']")
+use_web_app.send_keys(Keys.RETURN)
+
+
+
 
 
