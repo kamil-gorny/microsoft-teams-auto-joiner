ecb3dc7e21b69ef1e4cf953c8d7e7de781afe38e
Kamil <kamil.gorny99@gmail.com>
2020-11-29 15:48:37 +0100

    feat: Add list of teams to the submenu

--- autojoiner.py
+++ autojoiner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -17,7 +19,6 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 def enter_credentials():
     login = input("Enter login: ")
     password = input("Enter password: ")
-
     credentials = {"login": login, "password": password}
     f = open('credentials.json', "w")
     f.write(json.dumps(credentials))
@@ -46,18 +47,24 @@ def display_classes_register():
     menu.append_item(function_item)
     menu.show()
 
-
-def display_main_menu():
+def create_menu_items_for_teams(teams):
+    txt = open('autojoiner.txt', 'r', encoding='utf-8')
+    menu = ConsoleMenu(txt.read(),"")
+    for team in teams:
+        menu.append_item(FunctionItem(team, add_meeting))
+    menu.show()
+        
+def display_main_menu(teams):
     txt = open('autojoiner.txt', 'r', encoding='utf-8')
     menu = ConsoleMenu(txt.read(), "")
-    menu_item = FunctionItem("Enter credentials: ", enter_credentials)
-    function_item = FunctionItem("Scheadule meetings: ", display_classes_register)
+    menu_item = FunctionItem("Enter credentials ", enter_credentials)
+    function_item = FunctionItem("Scheadule meetings ", create_menu_items_for_teams, args=[teams])
     menu.append_item(menu_item)
     menu.append_item(function_item)
     menu.show()
 
 
-def try_locating_element(xpath):
+def try_locating_element(xpath, driver):
     try:
         WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, xpath))
@@ -67,18 +74,35 @@ def try_locating_element(xpath):
 
 
 def fill_and_move_to_the_next_step(driver, xpath, form_info):
-    try_locating_element(xpath)
+    try_locating_element(xpath, driver)
     form = driver.find_element_by_xpath(xpath)
     form.send_keys(form_info, Keys.RETURN)
 
  
 def get_teams(driver):
-    try_locating_element("//div[@class='team-card']")
+    try_locating_element("//div[@class='team-card']", driver)
     teams_elements = driver.find_elements_by_class_name('team-name-text')
     teams = []
     for element in teams_elements:
         teams.append(element.get_attribute('innerHTML'))
-    return teams 
+    return teams
+
+
+def sign_in(driver):
+    f = open('credentials.json', 'r')
+    credentials = json.load(f)
+    fill_and_move_to_the_next_step(driver,"//input[@id='i0116']", credentials['login'])
+    fill_and_move_to_the_next_step(driver, "//input[@id='i0118']",credentials['password'])
+    f.close()
+
+
+def check_for_credentials():
+    f =  open('credentials.json', 'r')
+    credentials = json.load(f)
+    if credentials['login'] != '' and credentials['password'] != '':
+        return True
+    else:
+        return False
 
 
 def main():
@@ -87,11 +111,14 @@ def main():
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(options=options, executable_path=PATH)
     driver.get("https://teams.microsoft.com")
-
-    menu_thread = threading.Thread(target=display_main_menu)
+    if check_for_credentials():
+        sign_in(driver)
+        teams = get_teams(driver)
+    menu_thread = threading.Thread(target=display_main_menu(teams))
     menu_thread.start()
-    fill_and_move_to_the_next_step(driver, "//input[@id='i0116']", "kamil")
-    fill_and_move_to_the_next_step(driver, "//input[@id='i0118']","pass")   
+
+    
+
 
 
 if __name__ == '__main__':
