5e2c35cb76234989eda81f7fb181dbc9b7a8b7af
Kamil <kamil.gorny99@gmail.com>
2020-12-01 20:55:08 +0100

    refactor: Change opening files to with open

--- autojoiner.py
+++ autojoiner.py
@@ -16,52 +16,53 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 
 
 def enter_credentials():
-    login = input("Enter login: ")
-    password = input("Enter password: ")
-    credentials = {"login": login, "password": password}
-    f = open('credentials.json', "w")
-    f.write(json.dumps(credentials))
-    f.close()
+    login = input('Enter login: ')
+    password = input('Enter password: ')
+    credentials = {'login': login, 'password': password}
+    with open('credentials.json', 'w') as f:
+        f = open('credentials.json', 'w')
+        f.write(json.dumps(credentials))
+ 
 
 
 def add_meeting():
-    team = input("Enter team name: ")
-    day = input("Enter day of the week: ")
-    time = input("Enter meeting start time: ")
-    chanel = input("Enter chanel name that meeting take place on: ")
-    meeting = {"team_name": team, "day": day, "time": time, "chanel": chanel}
-    f = open('meetings.json', "a")
-    f.write(json.dumps(meeting))
-    f.close
-
+    team = input('Enter team name: ')
+    day = input('Enter day of the week: ')
+    time = input('Enter meeting start time: ')
+    chanel = input('Enter chanel name that meeting take place on: ')
+    meeting = {'team_name': team, 'day': day, 'time': time, 'chanel': chanel}
+    with open('meetings.json', 'a') as f:
+        f.write(json.dumps(meeting))
+ 
 
 def display_classes_register():
-    txt=open('autojoiner.txt', 'r', encoding='utf-8')
-    menu = ConsoleMenu(txt.read(), "")
-    add_item = FunctionItem("Add meeting", add_meeting)
-    menu_item = FunctionItem("Show meetings", enter_credentials)
-    function_item = FunctionItem("Edit meetings", enter_credentials)
-    menu.append_item(add_item)
-    menu.append_item(menu_item)
-    menu.append_item(function_item)
-    menu.show()
+    with open('autojoiner.txt' ,'r', encoding='utf-8') as txt:
+        menu = ConsoleMenu(txt.read(), '')
+        add_item = FunctionItem('Add meeting', add_meeting)
+        menu_item = FunctionItem('Show meetings', enter_credentials)
+        function_item = FunctionItem('Edit meetings', enter_credentials)
+        menu.append_item(add_item)
+        menu.append_item(menu_item)
+        menu.append_item(function_item)
+        menu.show()
+
 
 def create_menu_items_for_teams(teams):
-    txt = open('autojoiner.txt', 'r', encoding='utf-8')
-    menu = ConsoleMenu(txt.read(),"")
-    for team in teams:
-        menu.append_item(FunctionItem(team, add_meeting))
-    menu.show()
-        
+    with open('autojoiner.txt', 'r', encoding='utf-8') as txt:
+        menu = ConsoleMenu(txt.read(),'')
+        for team in teams:
+            menu.append_item(FunctionItem(team, add_meeting))
+        menu.show()
+            
 
 def display_main_menu(teams):
-    txt = open('autojoiner.txt', 'r', encoding='utf-8')
-    menu = ConsoleMenu(txt.read(), "")
-    menu_item = FunctionItem("Enter credentials ", enter_credentials)
-    function_item = FunctionItem("Scheadule meetings ", create_menu_items_for_teams, args=[teams])
-    menu.append_item(menu_item)
-    menu.append_item(function_item)
-    menu.show()
+    with open('autojoiner.txt', 'r', encoding='utf-8') as txt:
+        menu = ConsoleMenu(txt.read(), '')
+        menu_item = FunctionItem('Enter credentials ', enter_credentials)
+        function_item = FunctionItem('Scheadule meetings ', create_menu_items_for_teams, args=[teams])
+        menu.append_item(menu_item)
+        menu.append_item(function_item)
+        menu.show()
 
 
 def try_locating_element(xpath, driver):
@@ -70,7 +71,7 @@ def try_locating_element(xpath, driver):
             EC.element_to_be_clickable((By.XPATH, xpath))
         )
     except:
-        print("Nie zlokalizowano")
+        print('Nie zlokalizowano')
 
 
 def fill_and_move_to_the_next_step(driver, xpath, form_info):
@@ -80,7 +81,7 @@ def fill_and_move_to_the_next_step(driver, xpath, form_info):
 
  
 def get_teams(driver):
-    try_locating_element("//div[@class='team-card']", driver)
+    try_locating_element('//div[@class='team-card']', driver)
     teams_elements = driver.find_elements_by_class_name('team-name-text')
     teams = []
     for element in teams_elements:
@@ -89,28 +90,27 @@ def get_teams(driver):
 
 
 def sign_in(driver):
-    f = open('credentials.json', 'r')
-    credentials = json.load(f)
-    fill_and_move_to_the_next_step(driver,"//input[@id='i0116']", credentials['login'])
-    fill_and_move_to_the_next_step(driver, "//input[@id='i0118']",credentials['password'])
-    f.close()
-
+    with open('credentials.json', 'r') as f:
+        credentials = json.load(f)
+        fill_and_move_to_the_next_step(driver,'//input[@id='i0116']', credentials['login'])
+        fill_and_move_to_the_next_step(driver, '//input[@id='i0118']',credentials['password'])
+        
 
 def check_for_credentials():
-    f =  open('credentials.json', 'r')
-    credentials = json.load(f)
-    if credentials['login'] != '' and credentials['password'] != '':
-        return True
-    else:
-        return False
+    with open('credentials.json', 'r') as f:
+        credentials = json.load(f)
+        if credentials['login'] != '' and credentials['password'] != '':
+            return True
+        else:
+            return False
 
 
 def main():
     options = webdriver.ChromeOptions()
     # options.headless = True 
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(options=options, executable_path=PATH)
-    driver.get("https://teams.microsoft.com")
+    driver.get('https://teams.microsoft.com')
     if check_for_credentials():
         sign_in(driver)
         teams = get_teams(driver)
@@ -118,9 +118,6 @@ def main():
     menu_thread.start()
 
     
-
-
-
 if __name__ == '__main__':
     main()
 
